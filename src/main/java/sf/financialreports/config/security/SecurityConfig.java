3a6e8d5766a502fa5812d902518f208141635575
Ruslan <54860448+heckfy88@users.noreply.github.com>
2025-05-04 18:04:33 +0300

    F-7: ручка GET /transactions

--- src/main/java/sf/financialreports/config/security/SecurityConfig.java
+++ src/main/java/sf/financialreports/config/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,7 +36,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(Customizer.withDefaults())
+                .csrf(csrf ->
+                        csrf.ignoringRequestMatchers("/api/auth/login")
+                )
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login").permitAll()
